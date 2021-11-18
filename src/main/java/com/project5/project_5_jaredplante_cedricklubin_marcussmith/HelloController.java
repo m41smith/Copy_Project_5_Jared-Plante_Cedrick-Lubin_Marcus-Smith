818cb2ca78cfdd8577ea502d8e3a5e515aa16458
jared <j2plante@student.bridgew.edu>
2021-11-18 14:47:34 -0500

    Initial commit

---
+++ src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/HelloController.java
@@ -0,0 +1,14 @@
+package com.project5.project_5_jaredplante_cedricklubin_marcussmith;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.project5.project_5_jaredplante_cedricklubin_marcussmith {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.project5.project_5_jaredplante_cedricklubin_marcussmith to javafx.fxml;
+    exports com.project5.project_5_jaredplante_cedricklubin_marcussmith;
+}
