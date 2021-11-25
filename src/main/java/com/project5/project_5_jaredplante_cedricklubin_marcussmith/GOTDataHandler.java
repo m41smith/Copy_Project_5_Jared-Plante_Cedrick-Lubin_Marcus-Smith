bab07bc00551fe01a0a6f4ce15d6f357cc64264f
jared <j2plante@student.bridgew.edu>
2021-11-25 15:51:22 -0500

    started GOTDataHandler and added libraries to module-info

---
+++ src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/GOTDataHandler.java
@@ -0,0 +1,12 @@
+package com.project5.project_5_jaredplante_cedricklubin_marcussmith;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
+public class GOTDataHandler {
+}
