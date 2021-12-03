af24616aa842726f33eee2e8e9de2fc02ce18b8b
jared <j2plante@student.bridgew.edu>
2021-12-03 16:38:18 -0500

    added the character names to the GOT window

--- src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/GOTDataHandler.java
+++ src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/GOTDataHandler.java
@@ -36,6 +36,47 @@ public class GOTDataHandler {
         var GOTData = jsonInterpreter.fromJson(responseBody, GOTDataType[].class);
         return GOTData;
     }
+    public GOTCharDataType[] getcharData(){
+        var httpbuilder = HttpRequest.newBuilder();
+        var dataRequest = httpbuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response= null;
+        try{
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }catch (IOException exception){
+            System.out.println("Error with the network");
+        }catch (InterruptedException e){
+            System.out.println("Error completing data transfer");
+        }
+        if(response == null){
+            System.out.println("Something went very wrong, quitting program");
+            System.exit(-1);
+        }
+        var responseBody = response.body();
+        var jsonInterpreter = new Gson();
+        var GOTCharData = jsonInterpreter.fromJson(responseBody, GOTCharDataType[].class);
+        return GOTCharData;
+    }
+    public GOTCharDataType getSingleCharData(){
+        var httpbuilder = HttpRequest.newBuilder();
+        var dataRequest = httpbuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response= null;
+        try{
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }catch (IOException exception){
+            System.out.println("Error with the network");
+        }catch (InterruptedException e){
+            System.out.println("Error completing data transfer");
+        }
+        if(response == null){
+            System.out.println("Something went very wrong, quitting program");
+            System.exit(-1);
+        }
+        var responseBody = response.body();
+        var jsonInterpreter = new Gson();
+        var GOTCharData = jsonInterpreter.fromJson(responseBody, GOTCharDataType.class);
+        return GOTCharData;
+    }
+
     class GOTDataType{
         String name;
         String isbn;
@@ -45,6 +86,16 @@ public class GOTDataHandler {
         public String toString(){
             return name;
 
+        }
+    }
+    class GOTCharDataType{
+        String name;
+        String born;
+        String died;
+        ArrayList<String> playedBy;
+        @Override
+        public String toString(){
+            return name;
         }
     }
 }
