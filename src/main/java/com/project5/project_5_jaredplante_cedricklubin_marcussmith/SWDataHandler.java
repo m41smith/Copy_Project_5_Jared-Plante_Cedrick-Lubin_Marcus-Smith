25c1709e5c37033324086f2ea7ac61a59aa749f4
m41smith <m41smith@student.bridgew.edu>
2021-12-08 17:01:51 -0500

    -got SWDataHandler working

--- src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/SWDataHandler.java
+++ src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/SWDataHandler.java
@@ -1,4 +1,56 @@
 package com.project5.project_5_jaredplante_cedricklubin_marcussmith;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
 public class SWDataHandler {
+    private HttpClient dataGrabber;
+    private String webLocation;
+
+    public SWDataHandler(String webLocation){
+        dataGrabber = HttpClient.newHttpClient();
+        this.webLocation = webLocation;
+    }
+
+    public StarWarsDataType getData() {
+        var httpbuilder = HttpRequest.newBuilder();
+        var dataRequest = httpbuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException exception) {
+            System.out.println("Error with the network");
+        }
+        catch (InterruptedException e) {
+            System.out.println("Error completing data transfer");
+        }
+        if (response == null) {
+            System.out.println("Something went very wrong, quitting program");
+            System.exit(-1);
+        }
+        var responseBody = response.body();
+        var jsonInterpreter = new Gson();
+        var StarWarsData = jsonInterpreter.fromJson(responseBody, StarWarsDataType.class);
+        return StarWarsData;
+    }
+
+    class StarWarsDataType{
+        String name;
+        String height;
+        String birth_year;
+        String homeworld;
+        ArrayList<String> films;
+
+        @Override
+        public String toString(){
+            return name;
+        }
+    }
 }
--- src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/SWWindowController.java
+++ src/main/java/com/project5/project_5_jaredplante_cedricklubin_marcussmith/SWWindowController.java
@@ -1,9 +1,14 @@
 package com.project5.project_5_jaredplante_cedricklubin_marcussmith;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,11 +21,36 @@ public class SWWindowController implements Initializable {
     @FXML
     private TextField DoB;
     @FXML
-    private ListView<SWDataHandler> ListControl;
+    private ListView<SWDataHandler.StarWarsDataType> ListControl;
+    private SWDataHandler Model;
+
+    public void loadData(){
+        var site = "https://swapi.dev/api/people/?search=";
+        var params = getQueryParams();
+        var query = site+params;
+        Model = new SWDataHandler(query);
+        var univList = Model.getData();
+        ObservableList<SWDataHandler.StarWarsDataType> dataToShow =
+                FXCollections.observableArrayList(univList);
+        ListControl.setItems(dataToShow);
+    }
+
+    private String getQueryParams() {
+        TextInputDialog inputGrabber = new TextInputDialog("Luke");
+        inputGrabber.setHeaderText("Gathering Information for query");
+        inputGrabber.setContentText("What Star Wars Character shall we search for:");
+        var name = inputGrabber.showAndWait();
+        if (name.isPresent()){
+            return name.get();
+        }
+        else {
+            return "";
+        }
+    }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        loadData();
     }
 }
